25700d0b889030c84b56b7aa814235c4b8088ced
Your Name <you@example.com>
2022-03-30 10:07:06 +0800

    fix 1.修正DB資料

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RouteSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(PointsSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
--- database/seeders/RouteSeeder.php
+++ database/seeders/RouteSeeder.php
@@ -16,7 +16,7 @@ class RouteSeeder extends Seeder
     public function run()
     {
         DB::table('routes')->insert([
-            [ 'id' => 100, 'name' => 'PRODUCT', 'parent_id' => 11, 'link' => NULL, 'icon' => "zmdi zmdi-shopping-cart", 'apilink' => NULL, 'seq' => 2, 'status' => 1, 'created_at' => Carbon::now()],
+            [ 'id' => 100, 'name' => 'PRODUCT', 'parent_id' => 0, 'link' => NULL, 'icon' => "zmdi zmdi-shopping-cart", 'apilink' => NULL, 'seq' => 2, 'status' => 1, 'created_at' => Carbon::now()],
             [ 'id' => 101, 'name' => 'PRODUCT_MANAGE', 'parent_id' => 100, 'link' => "Admin.Products.index", 'icon' => NULL, 'apilink' => NULL, 'seq' => 2, 'status' => 1, 'created_at' => Carbon::now()],
 	    ]);
     }
